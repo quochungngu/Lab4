3cca870c2cc39ba9d7b6bf4ddffb016073964048
HUNG <HUNG@HUNG-HP>
2017-11-01 21:16:43 -0400

    did fxml

--- FlamingoBLL/src/test/java/pkgCore/GamePlayTest.java
+++ FlamingoBLL/src/test/java/pkgCore/GamePlayTest.java
@@ -11,15 +11,18 @@ public class GamePlayTest {
 
 	@Test
 	public void GamePlayTest() {
+		
 		GamePlayBlackJack BlackJack = new GamePlayBlackJack(pkgEnum.eGameType.BLACKJACK);
 		HashMap<UUID,Player> players = new HashMap<UUID, Player>();
 		Player player1 = new Player("Shawn",0);
 		Player player2 = new Player("Hung", 1);
 		players.put(player1.getPlayerID(), player1);
 		players.put(player2.getPlayerID(), player2);
+		
 		BlackJack.AddPlayersToGame(players);
 		assertEquals(BlackJack.GetPlayerInGame(player1), player1);
 		assertEquals(BlackJack.GetPlayerInGame(player2), player2);
+		
 		BlackJack.RemovePlayerFromGame(player1);
 		BlackJack.RemovePlayerFromGame(player2);
 		assertNull(BlackJack.GetPlayerInGame(player1));
--- FlamingoBLL/src/test/java/pkgCore/TableTest.java
+++ FlamingoBLL/src/test/java/pkgCore/TableTest.java
@@ -10,9 +10,14 @@ public class TableTest {
 		Table table = new Table();
 		Player player1 = new Player("Shawn", 0);
 		Player player2 = new Player("Hung", 1);
+		
 		table.AddPlayerToTable(player1);
 		table.AddPlayerToTable(player2);
+		
 		assertEquals(table.GetPlayerFromTable(player1), player1);
+		assertEquals(table.GetPlayerFromTable(player2), player2);
+		
+		
 		table.RemovePlayerFromTable(player1);
 		assertNull(table.GetPlayerFromTable(player1));
 	}
